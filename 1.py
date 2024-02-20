47d9ade02f68966126e3a761e8d66d02d3377efe
9kitsune <38348408+9kitsune@users.noreply.github.com>
2024-02-20 22:29:54 +0000

    Update and rename test1.py to 1.py

--- 1.py
+++ 1.py
@@ -40,7 +40,7 @@ def get_chapters(transcript):
             ))
     return chapters 
 
-yt = YouTube("https://www.youtube.com/watch?v=fgZq_rnT18Q")
+yt = YouTube("https://www.youtube.com/watch?v=fgZq_rnT18Q") # edit for youtube link
 print(yt.title)
 it = innertube.InnerTube("WEB")
 data = it.next(yt.video_id)
@@ -48,39 +48,39 @@ params = extract_transcript_params(data)
 transcript = it.get_transcript(params)
 chapters = get_chapters(transcript)
 
-# yt.streams.get_highest_resolution().download()
-# # give 5 minutes to download the mp4
-# import os
-# import time
+yt.streams.get_highest_resolution().download()
+# give 5 minutes to download the mp4
+import os
+import time
 
-# time_to_wait = 300
-# time_counter = 0
-# while not os.path.exists('./*mp4'):
-#     time.sleep(1)
-#     time_counter += 1
-#     if time_counter > time_to_wait:break
+time_to_wait = 300
+time_counter = 0
+while not os.path.exists('./*mp4'):
+    time.sleep(1)
+    time_counter += 1
+    if time_counter > time_to_wait:break
 
-# # for chapter in yt.chapters:
-# #     fancy_start_tm = timedelta(seconds=chapter["timestamp"] / 1000.)
-# #     print(f"{fancy_start_tm} {chapter['label']}")
+# for chapter in yt.chapters:
+#     fancy_start_tm = timedelta(seconds=chapter["timestamp"] / 1000.)
+#     print(f"{fancy_start_tm} {chapter['label']}")
 
-# # call bash script to generate mp3 from mp4
-# from subprocess import call
-# rc = call('./mp42mp3.sh')
+# call bash script to generate mp3 from mp4
+from subprocess import call
+rc = call('./mp42mp3.sh')
 
-# # give 5 minutes to convert to mp3
-# import os
-# import time
+# give 5 minutes to convert to mp3
+import os
+import time
 
-# time_to_wait = 300
-# time_counter = 0
-# while not os.path.exists('./*mp3'):
-#     time.sleep(1)
-#     time_counter += 1
-#     if time_counter > time_to_wait:break
+time_to_wait = 300
+time_counter = 0
+while not os.path.exists('./*mp3'):
+    time.sleep(1)
+    time_counter += 1
+    if time_counter > time_to_wait:break
 
 from pydub import AudioSegment
-song = AudioSegment.from_mp3("./Hotel Del Luna OST.mp3") 
+song = AudioSegment.from_mp3("./*.mp3") # edit to the source mp3 file
 
 for x in chapters:
     t_start = x['start']*1000 # into milliseconds
